3ce46bbf8c9a536d2c976d1fef2318f7b60ba4f3
pjm <2556323541@qq.com>
2022-04-07 20:52:31 +0800

    Deployed c4547a2 with MkDocs version: 1.2.3

---
+++ algorithm/offer/code/cpp/25.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+
+        cur->next = (l1 == NULL ? l2 : l1);
+
+        return dummy->next;
+    }
+};
