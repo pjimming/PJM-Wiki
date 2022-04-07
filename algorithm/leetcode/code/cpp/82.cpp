e33f6b8388980f42a416e39c8ca8670f93b585c1
pjm <2556323541@qq.com>
2022-04-07 20:18:24 +0800

    Deployed 9251d13 with MkDocs version: 1.2.3

---
+++ algorithm/leetcode/code/cpp/82.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* p = dummy;
+        while (p->next) {
+            ListNode* q = p->next;
+            while (q && q->val == p->next->val)
+                q = q->next;
+            if (p->next->next == q) p = p->next;
+            else p->next = q;
+        }
+        return dummy->next;
+    }
+};
