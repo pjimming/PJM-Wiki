59366f714895257f7782401efb3bcf4e9b49b3e6
pjm <2556323541@qq.com>
2022-04-13 13:55:51 +0800

    leetcode 713

--- docs/js/mathjax.js
+++ docs/js/mathjax.js
@@ -24,3 +24,31 @@ window.MathJax = {
     loader: { load: ['[tex]/boldsymbol'] },
     tex: { packages: { '[+]': ['boldsymbol'] } }
 };
+
+window.MathJax = {
+    tex: {
+        macros: {
+            RR: "{\\bf R}",
+            bold: ["{\\bf #1}", 1]
+        },
+        environments: {
+            braced: ["\\left\\{", "\\right\\}"]
+        }
+    }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/configmacros'] },
+    tex: { packages: { '[+]': ['configmacros'] } }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/mathtools'] },
+    tex: { packages: { '[+]': ['mathtools'] } }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/textmacros'] },
+    tex: { packages: { '[+]': ['textmacros'] } }
+};
+
