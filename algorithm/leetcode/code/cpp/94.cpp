876af2da9b7bd13b6193151000215cac43943c38
pjm <2556323541@qq.com>
2022-06-27 12:01:36 +0800

    Deployed 91d492e with MkDocs version: 1.2.3

---
+++ algorithm/leetcode/code/cpp/94.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk;
+        TreeNode* p = root;
+        while (p || stk.size()) {
+            while (p) {
+                stk.push(p);
+                p = p->left;
+            }
+            p = stk.top();
+            stk.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
